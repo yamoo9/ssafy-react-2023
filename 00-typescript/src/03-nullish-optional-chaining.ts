53aca9964c4545561497f995b4134f4a04f75034
yamoo9 <yamoo9@naver.com>
2023-07-05 20:32:25 +0900

    스코프 이슈 해결
    모듈 파일이 아닌 경우, 글로벌 파일로 인식되기 때문에 블록 스코프로 감싸야 합니다.

--- 00-typescript/src/03-nullish-optional-chaining.ts
+++ 00-typescript/src/03-nullish-optional-chaining.ts
@@ -1,65 +1,67 @@
 // --------------------------------------------------------------------------
 // operators (nullish, optional chaining)
 
-function run() {
-  // nullish();
-  optionalChaining();
-}
-
-function nullish() {
-  let value = 0; // undefined
-
-  let result: number | undefined = value || 100;
-  console.log('|| : ', result);
-
-  function isNullOrUndefined(value: unknown) {
-    return value === null || value === undefined ? true : false;
+{
+  function run() {
+    // nullish();
+    optionalChaining();
   }
 
-  result = !isNullOrUndefined(value) ? value : 100;
-  console.log('isNullOrUndefined : ', result);
+  function nullish() {
+    let value = 0; // undefined
 
-  // 코드를 작성합니다.
-  // null 병합 연산자 활용
-  result = value ?? 100;
-  console.log('nullish : ', result);
-}
+    let result: number | undefined = value || 100;
+    console.log('|| : ', result);
 
-// 사용자 정의 타입
-type Topic = {
-  _title: string;
-  getTitle(): string;
-  setTitle(value: string): void;
-  getName?: () => string; // function | undefined
-};
-
-function optionalChaining() {
-  const topic: Topic = {
-    _title: '매년 업데이트 되는 ECMAScript',
-    getTitle() {
-      return this._title;
-    },
-    setTitle(value) {
-      this._title = value;
-    },
-  };
-
-  if (topic && typeof topic === 'object' && !Array.isArray(topic)) {
-    let title, name;
-    if (typeof topic.getTitle === 'function') {
-      title = topic.getTitle();
+    function isNullOrUndefined(value: unknown) {
+      return value === null || value === undefined ? true : false;
     }
-    // if (typeof topic.getName === 'function') {
-    // name = topic.getName();
-    // }
-    console.log('typeof : ', title);
-    console.log('typeof : ', name);
+
+    result = !isNullOrUndefined(value) ? value : 100;
+    console.log('isNullOrUndefined : ', result);
 
     // 코드를 작성합니다.
-    name = topic.getName?.();
+    // null 병합 연산자 활용
+    result = value ?? 100;
+    console.log('nullish : ', result);
   }
 
-  let title, name;
-}
+  // 사용자 정의 타입
+  type Topic = {
+    _title: string;
+    getTitle(): string;
+    setTitle(value: string): void;
+    getName?: () => string; // function | undefined
+  };
 
-run();
+  function optionalChaining() {
+    const topic: Topic = {
+      _title: '매년 업데이트 되는 ECMAScript',
+      getTitle() {
+        return this._title;
+      },
+      setTitle(value) {
+        this._title = value;
+      },
+    };
+
+    if (topic && typeof topic === 'object' && !Array.isArray(topic)) {
+      let title, name;
+      if (typeof topic.getTitle === 'function') {
+        title = topic.getTitle();
+      }
+      // if (typeof topic.getName === 'function') {
+      // name = topic.getName();
+      // }
+      console.log('typeof : ', title);
+      console.log('typeof : ', name);
+
+      // 코드를 작성합니다.
+      name = topic.getName?.();
+    }
+
+    let title, name;
+  }
+
+  run();
+}
