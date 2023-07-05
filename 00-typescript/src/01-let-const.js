fb91f35cfc4d2174eb84ac299f80f4c79fa9bd10
yamoo9 <yamoo9@naver.com>
2023-07-05 20:14:09 +0900

    TypeScript 학습
    - 타입 명시
    - 템플릿 리터럴
    - 블록 스코프 (let, const)

--- 00-typescript/src/01-let-const.js
+++
@@ -1,8 +0,0 @@
-// --------------------------------------------------------------------------
-// let, const (block scope)
-
-for (var i = 0, numbers = [3, 6, 9, 12]; i < numbers.length; ++i) {
-  // ...
-}
-
-console.log(i);
