cde36e4d6a647720fe6d9bcb14a0abf0dc1641fe
yamoo9 <yamoo9@naver.com>
2023-07-07 21:09:53 +0900

    React 학습
    - 점진적으로 사용하는 방법
    - 웹 브라우저 환경 (with CDN)
    - React, ReactDOM API

--- 02-react-babel-browser/scripts/main.js
+++ 02-react-babel-browser/scripts/main.js
@@ -1,5 +1,44 @@
 function main() {
-  console.log('HTML에서 React 시작하기! 😃');
+  const { createElement: h } = React;
+  const { createRoot } = ReactDOM;
+
+  // console.log('HTML에서 React 시작하기! 😃');
+
+  // React 요소 생성
+  const app = h(
+    'div',
+    {
+      className: 'app',
+    },
+    h(
+      'figure',
+      {
+        className: 'library',
+      },
+      h('img', {
+        src: 'assets/ReactLogo.svg',
+        alt: '',
+      }),
+      h(
+        'strong',
+        {
+          className: 'name',
+        },
+        'React'
+      ),
+      h(
+        'p',
+        {
+          className: 'catchphrize',
+        },
+        '사용자 인터페이스 구축을 위한 웹 &amp; 모바일 라이브러리'
+      )
+    )
+  );
+
+  // ReactDOMRoot 객체 생성
+  const reactDomRoot = createRoot(document.getElementById('root'));
+  reactDomRoot.render(app);
 }
 
 function handleGoToReference() {
