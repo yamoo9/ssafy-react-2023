34c0e332748e86c6681586f9efd0eb87d7977041
yamoo9 <yamoo9@naver.com>
2023-07-07 20:53:19 +0900

    함수형 프로그래밍 학습
    - 불변 데이터 관리
    - 순수 함수 vs. 불순 함수
    - 사이드 이펙트

--- 01-functional-programming/6-side-effects.js
+++ 01-functional-programming/6-side-effects.js
@@ -13,33 +13,53 @@
 // 아래 코드는 여러 기능이 복합적으로 엉켜 있어 순수하지 못합니다.
 // 코드를 "순수" 함수와 "부수 효과"를 처리하는 함수로 나눠 재구성합니다.
 
-function render(node) {
-  node.innerHTML = 'Loading...';
+function setLoading(isLoading = false, node) {
+  if (!node || node.nodeType !== document.ELEMENT_NODE) {
+    throw new Error('....');
+  }
 
-  fetch('https://random-data-api.com/api/v2/beers')
+  node.innerHTML = isLoading ? 'Loading...' : '';
+}
+
+function fetchData(endpoint) {
+  return fetch(endpoint)
     .then((response) => response.json())
-    .then((data) => {
-      const { brand, name, style, hop, malts, alchol } = data;
-      const beerElement = document.createElement('div');
-      beerElement.classList.add('beer');
-      beerElement.insertAdjacentHTML(
-        'beforeend',
-        /* html */ `
-          <h2>${brand} ${name}</h2>
-          <ul>
-            <li><b>style</b>: ${style}</li>
-            <li><b>hop</b>: ${hop}</li>
-            <li><b>malts</b>: ${malts}</li>
-            <li><b>alchol</b>: ${alchol}</li>
-          </ul>
-        `
-      );
-      node.innerHTML = '';
-      node.append(beerElement);
-    })
     .catch((error) => console.error(error.message));
+}
+
+function render(data, node) {
+  const { brand, name, style, hop, malts, alchol } = data;
+  const beerElement = document.createElement('div');
+  beerElement.classList.add('beer');
+  beerElement.insertAdjacentHTML(
+    'beforeend',
+    /* html */ `
+        <h2>${brand} ${name}</h2>
+        <ul>
+          <li><b>style</b>: ${style}</li>
+          <li><b>hop</b>: ${hop}</li>
+          <li><b>malts</b>: ${malts}</li>
+          <li><b>alchol</b>: ${alchol}</li>
+        </ul>
+      `
+  );
+
+  setLoading(false, node);
+
+  // node.append(beerElement);
+  node.insertAdjacentElement('beforeend', beerElement);
+}
+
+async function sideEffects(node) {
+  setLoading(true, node);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const data = await fetchData('https://random-data-api.com/api/v2/beers');
+
+  render(data, node);
 }
 
 const rootElement = document.getElementById('root');
 
-render(rootElement);
+sideEffects(rootElement);
