abf3e33861349b8725c939749ba6493d119eb38d
yamoo9 <yamoo9@naver.com>
2023-07-07 20:10:48 +0900

    ECMAScript 학습
    - class 구문
    - ES 모듈 (Node.js 환경)
    - package.json (type: 'module')

--- 00-typescript/scripts/13-class.js
+++ 00-typescript/scripts/13-class.js
@@ -1,43 +1,7 @@
 // --------------------------------------------------------------------------
 // ES5 Constructor (like. Class) with Module Pattern (IIFE)
 
-const ApiClient = (function () {
-  function ApiClient(endpoint) {
-    this._endpoint = endpoint;
-  }
-
-  ApiClient.prototype.readAll = function (page = 1, pageSize = 5) {
-    return fetch(`${this._endpoint}?_page=${page}&_limit=${pageSize}`).then(
-      (response) => response.json()
-    );
-  };
-
-  ApiClient.prototype.readOne = function (id) {
-    return fetch(`${this._endpoint}/${id}`).then((response) => response.json());
-  };
-
-  ApiClient.prototype.create = function (data) {
-    return fetch(this._endpoint, {
-      method: 'POST',
-      body: JSON.stringify(data),
-    }).then((response) => response.json());
-  };
-
-  ApiClient.prototype.update = function (id, data) {
-    return fetch(`${this._endpoint}/${id}`, {
-      method: 'PATCH',
-      body: JSON.stringify(data),
-    }).then((response) => response.json());
-  };
-
-  ApiClient.prototype.delete = function (id) {
-    return fetch(`${this._endpoint}/${id}`, {
-      method: 'DELETE',
-    }).then((response) => response.json());
-  };
-
-  return ApiClient;
-})();
+import ApiClient from './ApiClient.js';
 
 const todosService = new ApiClient(
   'https://jsonplaceholder.typicode.com/todos'
