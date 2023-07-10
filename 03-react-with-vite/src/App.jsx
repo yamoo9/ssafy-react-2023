e7b5de660250fb5324e1dcfc0cbf0dc92dee0228
yamoo9 <yamoo9@naver.com>
2023-07-10 19:53:05 +0900

    ESLint 플러그인 설치 및 구성 / 에셋 활용 방법
    - @eslint/config 설치 구성
    - public, src/assets 디렉토리 에셋 관리

--- 03-react-with-vite/src/App.jsx
+++ 03-react-with-vite/src/App.jsx
@@ -1,7 +1,15 @@
-const App = () => (
-  <div>
-    <h1 className="headline">React is Awesome!</h1>
-  </div>
-);
+import reactLogoPath from './assets/ReactLogo.svg';
+
+function App() {
+  return (
+    <div>
+      <h1 className="headline">React is Awesome!</h1>
+      <h2>정적 에셋</h2>
+      <img src="/ReactLogo.svg" alt="React Logo" />
+      <h2>동적 에셋</h2>
+      <img src={reactLogoPath}></img>
+    </div>
+  );
+}
 
 export default App;
--- 03-react-with-vite/src/main.jsx
+++ 03-react-with-vite/src/main.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-// ReactDOMRoot { render }
 createRoot(document.getElementById('root')).render(<App />);
