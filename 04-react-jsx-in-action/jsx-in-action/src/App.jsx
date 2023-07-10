617bb4509c986d34b20e7b21106e0a85401cb415
yamoo9 <yamoo9@naver.com>
2023-07-10 21:30:27 +0900

    React, JSX 인 액션
    - 데이터(콘텐츠, 속성, 불리언 속성) 바인딩
    - 조건부 렌더링 vs. 조건부 표시
    - 리스트 렌더링 (함수 반환 값, 배열 map 메서드 활용, key 값의 중요성)
    - 이벤트 핸들링 (DOM 요소 접근/조작 : 사이드 이펙트)

--- 04-react-jsx-in-action/jsx-in-action/src/App.jsx
+++ 04-react-jsx-in-action/jsx-in-action/src/App.jsx
@@ -1,9 +1,12 @@
-import './styles/App.css';
 /* eslint-disable no-unused-vars */
 
-// 스타일 및 에셋 연결
+import './styles/App.css';
 
-const imageType = 'react';
+// 스타일 및 에셋 연결
+import reactImage from './assets/react.svg';
+import viteImage from './assets/vite.svg';
+
+const imageType = 'vite'; // type ImageType = 'react' | 'vite'
 
 const isShowReactImage = true;
 
@@ -14,6 +17,17 @@ const statusMessage = [
   '❌ 로딩 실패.',
 ];
 
+const renderList = ({ reverse = false } = {}) => {
+  let processArray = statusMessage;
+
+  if (reverse) {
+    processArray = [...statusMessage].reverse();
+  }
+
+  return processArray.map((message, index) => <li key={index}>{message}</li>);
+};
+
+// 객체 리스트 렌더링
 const reactLibrary = {
   name: 'React',
   author: '조던 워케(Jordan Walke)',
@@ -24,25 +38,42 @@ const reactLibrary = {
 
 let mounted = 0;
 
-// 함수 컴포넌트가 순수한지 어떻게 체크할까?
-
 function App() {
-  const renderList = () => {
-    return null;
-  };
+  // console.log(++mounted); // 0 → 1
 
-  console.log(++mounted); // 0 → 1
+  // 변수 선언 -> 값
+  const statueMessageString =
+    statusMessage[Math.floor(Math.random() * statusMessage.length)];
+
+  const isntShowHeadline = false;
+
+  const isReactType = imageType === 'react';
+
+  // const viteImageStyles = { height: 42, objectFit: 'contain' };
+
+  const isShowReactImage = !true;
+
+  // function body area
+  // virtual dom
+  console.log(document.querySelector('.App')); // null
+
+  // life cycle methods
+  // side effects
+  // event handling (side effects)
 
   return (
     <div className="App">
-      <h1>JSX 인 액션</h1>
+      <h1 hidden={isntShowHeadline}>JSX 인 액션</h1>
       <hr />
       <dl className="descriptionList">
         <dt>데이터 바인딩(data binding)</dt>
         <dd>
           <p>상태 메시지(status message)를 연결해 화면에 출력합니다.</p>
-          <span className="status">
+          <span className="status" title={statueMessageString}>
             {/* statusMessage 값을 화면에 표시합니다. (랜덤 표시도 도전!) */}
+            {/* React Props.children [child, ...] */}
+            {/* JSX = JavaScript Expression */}
+            {statueMessageString}
           </span>
         </dd>
         <dt>조건부 렌더링(conditional rendering)</dt>
@@ -50,6 +81,17 @@ function App() {
           <p>이미지 타입(image type)에 따라 렌더링 여부를 결정합니다.</p>
           <div className="conditionalRendering">
             {/* imageType 값이 'vite'인 경우 Vite 이미지를, 'react'인 경우 React 이미지를 화면에 표시합니다. */}
+            {/* {imageType === 'vite' ? (
+              <img src={viteImage} alt="Vite" />
+            ) : (
+              <img src={reactImage} alt="react" />
+            )} */}
+            {/react|vite/.test(imageType) && (
+              <img
+                src={isReactType ? reactImage : viteImage}
+                alt={isReactType ? 'react' : 'vite'}
+              />
+            )}
             {/* imageType이 'vite'인 경우 'Vite', 'react'인 경우 'React' 텍스트를 화면에 표시합니다. */}
           </div>
         </dd>
@@ -58,8 +100,15 @@ function App() {
             spinner 또는 vite 이미지가 랜덤으로 화면에 렌더링 되도록 합니다.
           </p>
           <div className="conditionalRendering">
-            <img className="spinner" src="/spinner.svg" alt="로딩 중..." />
-            <img src="/vite.svg" alt="Vite" style={{ height: 42 }} />
+            {Math.random() >= 0.5 ? (
+              <img className="spinner" src="/spinner.svg" alt="로딩 중..." />
+            ) : (
+              <img
+                src="/vite.svg"
+                alt="Vite"
+                style={{ height: 42, objectFit: 'contain' }}
+              />
+            )}
           </div>
         </dd>
         <dt>조건부 표시(conditional display)</dt>
@@ -68,7 +117,7 @@ function App() {
             표시(display) 여부에 따라 이미지가 화면에서 감춰지거나 표시됩니다.
           </p>
           {/* isShowReactImage 상태 값에 따라 이미지가 화면에 표시되거나 표시되지 않도록 설정합니다. */}
-          <picture>
+          <picture hidden={!isShowReactImage}>
             <source type="image/avif" srcSet="/react.avif" />
             <source type="image/webp" srcSet="/react.webp" />
             <img src="/react.png" alt="React" height={42} />
@@ -78,10 +127,24 @@ function App() {
         <dd>
           <p>상태 메시지(status message) 배열을 리스트 렌더링합니다.</p>
           <ul className="renderList">{renderList()}</ul>
+          {/* <ul className="renderList">
+            {statusMessage.map((message, index) => {
+              return <li key={index}>{message}</li>;
+            })}
+          </ul> */}
         </dd>
         <dd>
           <p>상태 메시지(status message) 배열을 역순 정렬하여 렌더링합니다.</p>
           <ul className="renderList">{renderList({ reverse: true })}</ul>
+          {/* <ul className="renderList">
+            {[...statusMessage].reverse().map((message, index) => {
+              return <li key={index}>{message}</li>;
+            })}
+          </ul> */}
+          {/* <p>
+            statusMessage:
+            {statusMessage}
+          </p> */}
         </dd>
         <dd>
           <p>
@@ -90,6 +153,15 @@ function App() {
           </p>
           <dl className="reactLibrary">
             {/* 여기서 설명 목록으로 리스트 렌더링 합니다. */}
+            {/* 객체 → 배열 */}
+            <dl>
+              {Object.entries(reactLibrary).map(([key, value]) => (
+                <div key={key}>
+                  <dt>{key}</dt>
+                  <dd>{value}</dd>
+                </div>
+              ))}
+            </dl>
           </dl>
         </dd>
       </dl>
@@ -101,6 +173,18 @@ function App() {
           className="scrollDown"
           aria-label="스크롤 다운"
           title="스크롤 다운"
+          onClick={(e) => {
+            // console.log('clicked scroll down button');
+            const app = document.querySelector('.App');
+            const buttonGroup = document.querySelector(
+              '.buttonGroup[role=group]'
+            );
+
+            app?.scroll({
+              top: buttonGroup?.getBoundingClientRect().top,
+              behavior: 'smooth',
+            });
+          }}
         >
           <svg
             fill="currentColor"
@@ -124,6 +208,14 @@ function App() {
           className="scrollUp"
           aria-label="스크롤 업"
           title="스크롤 업"
+          onClick={() => {
+            // console.log('clicked scroll up button');
+            const app = document.querySelector('.App');
+            app?.scroll({
+              top: 0,
+              behavior: 'smooth',
+            });
+          }}
         >
           <svg
             fill="currentColor"
--- 04-react-jsx-in-action/jsx-in-action/src/main.jsx
+++ 04-react-jsx-in-action/jsx-in-action/src/main.jsx
@@ -5,6 +5,12 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 
+// [개발 모드 활성 : React에서 컴포넌트의 순수함을 확인하는 방법]
+// 참고: https://react.dev/reference/react/StrictMode
+// INPUT (props) -> COMPONENT -> OUTPUT (JSX) ⌝
+//                                         동일해야 함
+// INPUT (props) -> COMPONENT -> OUTPUT (JSX) ⌟
+
 createRoot(rootElement).render(
   <StrictMode>
     <App />
