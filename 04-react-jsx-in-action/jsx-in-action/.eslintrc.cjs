6cfe74b063239bacd1a26ebfa18c78bd2baded2a
yamoo9 <yamoo9@naver.com>
2023-07-12 19:36:44 +0900

    Vite 앱에 ESLint 플러그인 추가

--- 04-react-jsx-in-action/jsx-in-action/.eslintrc.cjs
+++ 04-react-jsx-in-action/jsx-in-action/.eslintrc.cjs
@@ -1,16 +1,15 @@
-/* eslint-env node */
-
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jsx-a11y'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
