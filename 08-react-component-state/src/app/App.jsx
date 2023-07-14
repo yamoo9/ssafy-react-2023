55b840bff4b08e0943cff6f41bdcc405a13b0be5
yamoo9 <yamoo9@naver.com>
2023-07-14 20:46:38 +0900

    React 상태 관리 Part 2.
    - `key` prop이 변경되면?
    - 컴포넌트는 React에 의해 다시 렌더링 됩니다.

--- 08-react-component-state/src/app/App.jsx
+++ 08-react-component-state/src/app/App.jsx
@@ -3,26 +3,12 @@ import React from 'react';
 import styles from './App.module.css';
 import { ScrollButton, DescriptionList } from '@/components';
 
-const isShowReactImage = true;
-
-const statusMessage = [
-  '⌛️ 대기',
-  '⏳ 로딩 중...',
-  '✅ 로딩 성공!',
-  '❌ 로딩 실패.',
-];
-
 const reactLibrary = {
   name: 'React',
   author: '조던 워케(Jordan Walke)',
   writtenIn: 'JavaScript',
   type: 'JavaScript 라이브러리',
   license: 'MIT',
-};
-
-const renderList = ({ isReverse = false } = {}) => {
-  const data = !isReverse ? statusMessage : statusMessage.reverse();
-  return data.map((message, index) => <li key={index}>{message}</li>);
 };
 
 const handleScrollMove = ({ currentTarget, target }) => {
@@ -53,23 +39,58 @@ const handleScrollMove = ({ currentTarget, target }) => {
 
 function App() {
   const [imageType, setImageType] = React.useState('react');
+  const [isShowReactImage, setIsShowReactImage] = React.useState(true);
+
+  const [descKey, setDescKey] = React.useState(9);
+  const [statusMessage] /* read-only */ = React.useState([
+    '⌛️ 대기',
+    '⏳ 로딩 중...',
+    '✅ 로딩 성공!',
+    '❌ 로딩 실패.',
+  ]);
+
+  const renderList = ({ isReverse = false } = {}) => {
+    const data = !isReverse ? statusMessage : statusMessage.reverse();
+    return data.map((message, index) => <li key={index}>{message}</li>);
+  };
 
   return (
     <div className={styles.container}>
       <h1>React 컴포넌트 상태 관리</h1>
-      <button
-        type="button"
-        onClickCapture={() => {
-          // DOM 요소 접근/조작
-          // 사이드 이펙트 처리
-          // 선언된 상태 업데이트 → UI 리-렌더링
-          setImageType(imageType === 'react' ? 'vite' : 'react');
-        }}
-      >
-        change imageType
-      </button>
+      <div style={{ display: 'flex', columnGap: 8 }}>
+        <button
+          type="button"
+          onClick={() => {
+            const newDescKey = descKey + 3;
+            setDescKey(newDescKey);
+            console.log({ newDescKey });
+          }}
+        >
+          reload DescriptionList component
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setIsShowReactImage(!isShowReactImage);
+          }}
+        >
+          {isShowReactImage ? '리액트 이미지 감춤' : '리액트 이미지 표시'}
+        </button>
+        <button
+          type="button"
+          onClickCapture={() => {
+            // DOM 요소 접근/조작
+            // 사이드 이펙트 처리
+            // 선언된 상태 업데이트 → UI 리-렌더링
+            setImageType(imageType === 'react' ? 'vite' : 'react');
+          }}
+        >
+          change imageType
+        </button>
+      </div>
       <hr />
       <DescriptionList
+        key={descKey}
         {...{
           statusMessage,
           imageType,
